749a3198ad680a4982e83318326e66ad81bcd880
satoru <satoru_oshiro@kunisys.co.jp>
2023-03-20 16:56:06 +0900

    upd bunbougu

--- laravel/bunbougu/resources/views/layouts/navigation.blade.php
+++ laravel/bunbougu/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('bunbougus.index')" :active="request()->routeIs('bunbougus.index')">
+                        {{ __('bunbougus') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
